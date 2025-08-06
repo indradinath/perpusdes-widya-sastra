af4a4189d50e7c912f77335a58d66388cf0361c7
Indra Dinath <indradinath@gmail.com>
2025-08-06 10:47:41 +0800

    frontend1 script change

--- resources/views/frontend1/script.blade.php
+++ resources/views/frontend1/script.blade.php
@@ -2,4 +2,4 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
 <script src="{{asset('/public/template/js/scripts.js')}}"></script>
-<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+{{-- <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script> --}}
